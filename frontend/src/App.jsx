ac99e58cd9e4557d5d1deda082c32878b2f341e6
Likeith T M <likeithtakel@gmail.com>
2026-04-29 08:46:15 +0530

    Update import path for CONTRACT_ADDRESS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useCallback } from "react";
 import { useAccount } from "wagmi";
 import { useContract } from "./hooks/useContract";
 import { usePolicies } from "./hooks/usePolicies";
-import { CONTRACT_ADDRESS } from "./utils/constants";
+import { CONTRACT_ADDRESS } from "frontend/src/utils/constants.js";
 
 import AnimatedBackground from "./components/AnimatedBackground";
 import Navbar            from "./components/Navbar";
